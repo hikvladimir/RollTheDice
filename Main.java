adb1485f81540f196b2068aa2ff26964308e6b7c
hikvladimir <psihouse10@gmail.com>
2024-10-15 18:38:51 +0300

    step3

--- Main.java
+++ Main.java
@@ -1,55 +1,51 @@
 
-
+import java.util.Random;
 import java.util.Scanner;;
 
 public class Main {
     public static void main(String[] args) {
-        int sicret;
-        int pop;
 
+         int pop=0;
         Scanner in=new Scanner(System.in);
         int chislo=0;
         System.out.println("Угадай число от 1 до 100");
         String  str="";
-        String str2="";
-
+        String str2;
         do{
             int max=100;
-            sicret=(int)(Math.random()*++max);
+           int  sicret=(int)(Math.random()*++max);
+            while (sicret!=chislo) {
+                System.out.print("Введи число: ");
+                str= in.next();
+                if (str.equals("RESULT")){
+                    System.out.println("Количество попыток "+pop);
+                    continue;
+                }
+                try {
+                    chislo=Integer.parseInt(str);
+                } catch (Exception e){
+                    System.out.println("Нет это не число от 0 до 100");
+                    continue;
+                }
+                if (sicret < chislo) {
+                    System.out.println("Я сам в шоке, но, загаданное число меньше, брат");
+                } else if (sicret > chislo) {
+                    System.out.println("Не ожидал от тебя такого. Загаданное число больше, брат");
+                }
+                pop++;
+            }
+
+            System.out.println("Поздравляю! Вы угадали число " + sicret +" за "+pop+" попыток");
+
             System.out.println("Играем еще?");
             str2=in.next();
         } while (str2.equals("y"));
-        do {
-
-
-            pop=0;
-            System.out.print("Введи число: ");
-            str= in.next();
-            if (str.equals("RESULT")){
-                System.out.println("Количество попыток "+pop);
-                continue;
-            }
-            try {
-                chislo=Integer.parseInt(str);
-            } catch (Exception e){
-                System.out.println("Нет это не число от 0 до 100");
-                continue;
-            }
-            if (sicret < chislo) {
-                System.out.println("Я сам в шоке, но, загаданное число меньше, брат");
-            } else if (sicret > chislo) {
-                System.out.println("Не ожидал от тебя такого. Загаданное число больше, брат");
-            }
-            pop++;
-        }
-       while (sicret!=chislo);
-        System.out.println("Поздравляю! Вы угадали число " + sicret +" за "+pop+" попыток");
-
-        System.out.println("Сыграем еще?");
-        str2=in.next();
 
 
 
+
+
+        in.close();
     }
 
 }
